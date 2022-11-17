05adface7e9ac3a0888e82546bfef3656fe0500e
molevision <angadmishra@outlook.com>
2022-11-17 00:49:14 -0800

    Underline and more content

--- script.js
+++ script.js
@@ -5,26 +5,26 @@ const slideCount = slides.length;
 const slideWidth = 540;
 
 const highlightThumbnail = () => {
-  thumbnailContainer
-    .querySelectorAll('div.highlighted')
-    .forEach(el => el.classList.remove('highlighted'));
-  const index = Math.floor(slideGallery.scrollLeft / slideWidth);
-  thumbnailContainer
-    .querySelector(`div[data-id="${index}"]`)
-    .classList.add('highlighted');
+	thumbnailContainer
+		.querySelectorAll('div.highlighted')
+		.forEach(el => el.classList.remove('highlighted'));
+	const index = Math.floor(slideGallery.scrollLeft / slideWidth);
+	thumbnailContainer
+		.querySelector(`div[data-id="${index}"]`)
+		.classList.add('highlighted');
 };
 
 const scrollToElement = el => {
-  const index = parseInt(el.dataset.id, 10);
-  slideGallery.scrollTo(index * slideWidth, 0);
+	const index = parseInt(el.dataset.id, 10);
+	slideGallery.scrollTo(index * slideWidth, 0);
 };
 
 thumbnailContainer.innerHTML += [...slides]
-  .map((slide, i) => `<div data-id="${i}"></div>`)
-  .join('');
+	.map((slide, i) => `<div data-id="${i}"></div>`)
+	.join('');
 
 thumbnailContainer.querySelectorAll('div').forEach(el => {
-  el.addEventListener('click', () => scrollToElement(el));
+	el.addEventListener('click', () => scrollToElement(el));
 });
 
 slideGallery.addEventListener('scroll', e => highlightThumbnail());
@@ -32,28 +32,38 @@ slideGallery.addEventListener('scroll', e => highlightThumbnail());
 highlightThumbnail();
 
 
-
-
 function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+	// Hide all elements with class="tabcontent" by default */
+	var i, tabcontent, tablinks, underline;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	
+	// Remove the background color of all tablinks/buttons
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].style.backgroundColor = "";
+	}
+	
+	// Show the specific tab content
+	document.getElementById(pageName).style.display = "block";
 
-  // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-
-  // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
-
-  // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
-  elmnt.style.borderRadius = "20px";
+	// Add the specific color to the button used to open the tab content
+	// elmnt.style.backgroundColor = color;
+	// elmnt.style.borderRadius = "20px";
+	
+	
+	underline = document.getElementsByClassName("underline");
+	for (i = 0; i < underline.length; i++) {
+		underline[i].style.opacity = "0";
+	}
+	
+	underline = elmnt.getElementsByClassName("underline");
+	
+	for (i = 0; i < underline.length; i++) {
+		underline[i].style.opacity = "1";
+	}
 }
 
 // Get the element with id="defaultOpen" and click on it
